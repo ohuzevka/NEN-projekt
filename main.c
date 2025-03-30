8f9d83aab2d782e38aeb199c5019b1aca814c9cc
ohuzevka <95550595+ohuzevka@users.noreply.github.com>
2025-03-30 22:10:03 +0200

    Improved readability

--- main.c
+++ main.c
@@ -17,36 +17,57 @@
  * RD7 - High side motor
  */
 
-#define BUTTON_PIN (PORTDbits.RD1)
-#define MOTOR_NEG_PIN (PORTDbits.RD2)
-#define MOTOR_POS_PIN (PORTDbits.DR7)
-#define LED_PIN (PORTDbits.RD5)
-#define OPT_SENSOR_PIN (PORTCbits.RC5)
+#define BUTTON_PIN      (PORTDbits.RD1)
+#define LED_PIN         (PORTDbits.RD5)
+#define MOTOR_NEG_PIN   (PORTDbits.RD2)
+#define MOTOR_POS_PIN   (PORTDbits.RD7)
+#define OPT_SENSOR_PIN  (PORTCbits.RC5)
 
 /*
  * 
  */
 int main(int argc, char** argv) {
     
-    TRISD |= (1<<1);    // RD1 input 
-    TRISC |= (1<<5);    // RC5 input 
-    TRISD &= ~(1<<2);    // RD2 output
-    TRISD &= ~(1<<7);    // RD7 output
-    TRISD &= ~(1<<5);    // RD5 output
+    // TRISD |= (1<<1);    // RD1 input 
+    // TRISC |= (1<<5);    // RC5 input 
+    // TRISD &= ~(1<<2);    // RD2 output
+    // TRISD &= ~(1<<7);    // RD7 output
+    // TRISD &= ~(1<<5);    // RD5 output
+
+    // PORTD |= (1<<2);    // RD2 write HIGH
     
-    PORTD |= (1<<2);    // RD2 write HIGH
+    // while(1) {
+    //     if (PORTDbits.RD1){    // IF RD1 is HIGH
+    //         PORTD &= ~(1<<7);    // Write LOW to RD7
+    //     } else {
+    //         PORTD |= (1<<7);    // Write HIGH to RD7
+    //     }
+        
+    //     if (PORTCbits.RC5){    // IF RB7 is HIGH
+    //         PORTD |= (1<<5);    // Write HIGH to RD5
+    //     } else {
+    //         PORTD &= ~(1<<5);    // Write LOW to RD5
+    //     }
+    // }
+
+    // Port setup
+    TRISD |= _TRISD_TRISD1_MASK | _TRISD_TRISD5_MASK;                           // Set TRISD pins as input
+    TRISD &= ~(_TRISD_TRISD2_MASK | _TRISD_TRISD5_MASK | _TRISD_TRISD7_MASK);   // Set TRISD pins as output
+    TRISC |= _TRISC_TRISC5_MASK;                                                // Set TRISC pins as input
     
+    MOTOR_NEG_PIN = 1;  // Activate LOW side transistor to connect motor negative pin to GND
+
     while(1) {
-        if (PORTDbits.RD1){    // IF RD1 is HIGH
-            PORTD &= ~(1<<7);    // Write LOW to RD7
+        if (BUTTON_PIN == 0){   // If button is pressed
+            MOTOR_POS_PIN = 1;  // Turn motor ON
         } else {
-            PORTD |= (1<<7);    // Write HIGH to RD7
+            MOTOR_POS_PIN = 0;  // Turn motor OFF
         }
         
-        if (PORTCbits.RC5){    // IF RB7 is HIGH
-            PORTD |= (1<<5);    // Write HIGH to RD5
+        if (OPT_SENSOR_PIN){    // If optical sensor is high
+            LED_PIN = 1;        // Turn ON LED
         } else {
-            PORTD &= ~(1<<5);    // Write LOW to RD5
+            LED_PIN = 0;        // Turn OFF LED
         }
     }
     
