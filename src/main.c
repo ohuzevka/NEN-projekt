9ce1a377c88176d44fe20b8ae739840ea109610f
ohuzevka <95550595+ohuzevka@users.noreply.github.com>
2025-04-15 17:03:41 +0200

    workink +-

--- src/main.c
+++ src/main.c
@@ -10,9 +10,13 @@
 #define MOTOR_POS_PIN   (PORTDbits.RD7)
 #define OPT_SENSOR_PIN  (PORTAbits.RA4) // T0CKI pin
 
-#define PWM_REG CCPR2L  // Register to set PWM DC [%]   
+#define PWM_REG CCPR2L  // Register to set PWM DC [%] 
 
-unsigned char SW2_pressed, SW3_pressed;
+#define PWM_STEP 5
+
+volatile unsigned char SW2_pressed, SW3_pressed;
+volatile unsigned char refresh_display_flag = 0;
+volatile unsigned char RPS = 0;
 
 void DisplayNumber(unsigned int number){
     unsigned char nmr1, nmr2, nmr3, nmr4;
@@ -39,8 +43,7 @@ void main(void)
     TRISDbits.TRISD7 = 0;   // MOTOR_POS_PIN setup
     TRISAbits.TRISA4 = 1;   // OPT_SENSOR_PIN setup
     
-
-    // TIM1 setup as 1 second timer
+    // TIM1 setup as 1 seconds timer
     T1CONbits.TMR1CS = 0;   // Internal clock (F_OSC/4)
     T1CONbits.T1SYNC = 1;   // Do not synchronize external clock input
     // T1CONbits.T1CKPS = 0x01;// Prescaler 1:2 -> fosc/4 -> 1us * 2 = 2us
@@ -59,7 +62,6 @@ void main(void)
     INTCONbits.GIE = 1;
 
 
-
     // TIM0 setup as counter
     OPTION_REGbits.T0CS = 1;    // External clock from T0CKI pin
     OPTION_REGbits.T0SE = 0;    // Increment on low-to-high transition on T0CKI pin
@@ -67,19 +69,23 @@ void main(void)
     
     // TIMER2 for PWM
     // T2CON = 0x05;			// fosc/4 -> 0,5us * 4 = 2us
-    T2CONbits.T2CKPS1 = 0;
-    T2CONbits.T2CKPS0 = 1;
+    T2CONbits.T2CKPS1 = 0;  // Set prescaler to 4
+    T2CONbits.T2CKPS0 = 1;  // Set prescaler to 4
     T2CONbits.TMR2ON = 1;   // TIM2 ON
 
  	PR2 = 100;          	// 2u * 100 = 200us === PERIODE
  	PIR1bits.TMR2IF = 0;    // iclear interrupt flag
  	PIE1bits.TMR2IE = 0;    // interrupt disable
     
-
     //PWM2
     CCP2CON = 0x0C;         // LSB bits 0, PWM mode
     PWM_REG = 50;           // to register of duty cycle
     
+
+    // UART init
+    TXSTAbits.TXEN = 1;     // Enable TX
+    TXSTAbits.SYNC = 0;     // Assynchnonoous operation
+    RCSTAbits.SPEN = 1;     // Enable AUSART
 
     MOTOR_NEG_PIN = 1;      // Activate LOW side transistor to connect motor negative pin to GND
     initLcdDisplay();
@@ -88,11 +94,15 @@ void main(void)
     while(1) {
         if (SW2_pressed){
             SW2_pressed = 0;
-            PWM_REG -= 5;
+            if ((PWM_REG - PWM_STEP) != 0) {
+                PWM_REG -= PWM_STEP;
+            }
         }
         if (SW3_pressed) {
             SW3_pressed = 0;
-            PWM_REG += 5;
+            if ((PWM_REG + PWM_STEP) <= 100) {
+                PWM_REG += PWM_STEP;
+            }
         }
         
         if (SW4_PIN == 0) {
@@ -108,11 +118,16 @@ void main(void)
             LED_PIN = 0;        // Turn OFF LED
         }
         // LED_PIN = OPT_SENSOR_PIN;
+
+        if (refresh_display_flag) {
+            refresh_display_flag = 0;
+            DisplayNumber(RPS);
+        }
     }
 }
 
 
-unsigned char SW2_last, SW3_last;
+volatile unsigned char SW2_last, SW3_last;
 
 // interrupt TMR1
 unsigned char interrupt_cnt = 0;
@@ -126,9 +141,9 @@ void __interrupt() isr()
 
         // Button debounce
         if (SW2_last == 1 && SW2_PIN == 0)
-        SW2_pressed = 1;
+            SW2_pressed = 1;
         if (SW3_last == 1 && SW3_PIN == 0)
-        SW3_pressed = 1;
+            SW3_pressed = 1;
         
         SW2_last = SW2_PIN;
         SW3_last = SW3_PIN;
@@ -136,7 +151,9 @@ void __interrupt() isr()
         if (interrupt_cnt++ >= 7) {   // divider by 8, executes every 1 s
             interrupt_cnt = 0;
             
-            DisplayNumber(TMR0);
+            // DisplayNumber(TMR0);
+            RPS = TMR0;
+            refresh_display_flag = 1;
             TMR0 = 0;
         }
     }
